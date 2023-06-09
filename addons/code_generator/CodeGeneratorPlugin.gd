41e0ebacc7ee05f183d1f43cba8ee90ce415778d
coderbloke <75695649+coderbloke@users.noreply.github.com>
2023-06-09 21:39:38 +0200

    Needed to turn into a plugin, otherwise editor doesn't show the class names in resource creation dialog

---
+++ addons/code_generator/CodeGeneratorPlugin.gd
@@ -0,0 +1,12 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree():
+	# Does nothing. But needed to be a plug-in.
+	# Otherwise the classes are not listed in the resource creating dialog. 
+	pass
+
+
+func _exit_tree():
+	pass
